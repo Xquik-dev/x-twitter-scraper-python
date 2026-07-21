f700bac5d9163ff0d0f8d41694b8414768c34082
kriptoburak <kriptoburak@users.noreply.github.com>
2026-07-21 07:47:39 +0300

    Fix Atheris fuzz packaging

--- .clusterfuzzlite/build.sh
+++ .clusterfuzzlite/build.sh
@@ -2,7 +2,9 @@
 
 export PYTHONPATH="$SRC/x-twitter-scraper-python/src"
 
-compile_python_fuzzer "$SRC/x-twitter-scraper-python/.clusterfuzzlite/fuzz_querystring.py"
+compile_python_fuzzer \
+  "$SRC/x-twitter-scraper-python/.clusterfuzzlite/fuzz_querystring.py" \
+  --hidden-import atheris
 
 cat > "$OUT/fuzz_querystring.options" <<'EOF'
 [libfuzzer]
